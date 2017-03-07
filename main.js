f16e1a7d76f33b1bebe21fb258740eb8e92834b2
Patrick Akpala <kemaakpala@gmail.com>
2017-03-07 09:52:14 +0000

    truncate file

--- main.js
+++ main.js
@@ -242,39 +242,83 @@ var fs = require("fs");
 // });
 
 var buf = new Buffer(1024);
+// console.log("Going to open an existing file");
+// fs.open('input.txt', 'r+', function(err, fd) {
+//   if(err){
+//     return console.error(err);
+//   }
+//   console.log("File opened successfully!");
+//   console.log("Going to read the file");
+//   /*
+//   fs.read(  fd, // this is the file descriptor returned by fs.open().
+//             buffer, // this is the buffer that data will be writted to.
+//             offset, // this is the offset in the buffer to start writing at.
+//             length, // this is the number of bytes to read.
+//             position, // this is an integer that specifies where to begin reading from. {0,null- data will be read from the current file position}
+//             callback // this is the callback function which gets the 3 arguments, (err, bytesRead, buffer).
+//   );
+//   */
+//   fs.read(fd, buf, 0, buf.length, 0, function(err, bytes){
+//     if(err){
+//       console.log(err);
+//     }
+//     console.log(bytes + " bytes read");
+//
+//     //Print only read bytes to avoid junk.
+//     if(bytes > 0){
+//       console.log(buf.slice(0, bytes).toString());
+//     }
+//
+//     // Close the opened file.
+//     /*
+//     fs.close ( fd, //this is the file descriptor returned by fs.open().
+//                 callback // this is the callback function only the exception argument is given to it.
+//     );
+//     */
+//     fs.close(fd, function(err){
+//       if (err){
+//         console.log(err);
+//       }
+//       console.log("File closed successfully.")
+//     });
+//   });
+// });
+
 console.log("Going to open an existing file");
-fs.open('input.txt', 'r+', function(err, fd) {
+fs.open('input.txt', 'r+', function(err, fd){
   if(err){
     return console.error(err);
   }
   console.log("File opened successfully!");
-  console.log("Going to read the file");
-  /*
-  fs.read(  fd, // this is the file descriptor returned by fs.open().
-            buffer, // this is the buffer that data will be writted to.
-            offset, // this is the offset in the buffer to start writing at.
-            length, // this is the number of bytes to read.
-            position, // this is an integer that specifies where to begin reading from. {0,null- data will be read from the current file position}
-            callback // this is the callback function which gets the 3 arguments, (err, bytesRead, buffer).
-  );
-  */
-  fs.read(fd, buf, 0, buf.length, 0, function(err, bytes){
+  console.log("Going to truncate the file after 10 bytes");
+
+  // Truncate the opened file.
+  fs.ftruncate(fd, 10, function(err){
     if(err){
       console.log(err);
     }
-    console.log(bytes + " bytes read");
+    console.log("File truncated successfully.");
+    console.log("Going to read the same file");
 
-    //Print only read bytes to avoid junk.
-    if(bytes > 0){
-      console.log(buf.slice(0, bytes).toString());
-    }
-
-    // Close the opened file.
-    fs.close(fd, function(err){
-      if (err){
+    fs.read(fd, buf, 0, buf.length, 0, function(err, bytes){
+      if(err){
         console.log(err);
       }
-      console.log("File closed successfully.")
+
+      // Print only read bytes to avoid junk.
+      if(bytes > 0){
+        console.log(buf.slice(0, bytes).toString())
+      }
+
+      // Close the opened file.
+      fs.close(fd, function(err){
+        if(err){
+          console.log(err);
+        }
+        console.log("File closed successfully.");
+      });
     });
   });
+
+
 });
