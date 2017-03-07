4a5e0116e7762c4a0a58d9b2cc2908d7f183f0ec
Patrick Akpala <kemaakpala@gmail.com>
2017-03-07 09:15:49 +0000

    file manipulation section

--- main.js
+++ main.js
@@ -66,7 +66,7 @@ eventEmitter.emit('connection');
 console.log('Program Ended');
 */
 
-
+/*
 var events = require('events');
 var eventEmitter = new events.EventEmitter();
 
@@ -91,7 +91,6 @@ console.log(eventListeners + "Listner(s) listening to connection event");
 
 //Fire the connection event
 eventEmitter.emit('connection');
-
 //Remove the binding of Listner1 function
 eventEmitter.removeListener('connection', listner1);
 console.log("Listner1 will not listen now.");
@@ -102,4 +101,180 @@ eventEmitter.emit('connection');
 eventListeners = require('events').EventEmitter.listenerCount(eventEmitter, 'connection');
 console.log(eventListeners + "Listener(s) listening to connection event");
 
-console.log("Program Ended.");
+console.log("Program Ended.");*/
+
+//Reading from a Stream
+// var fs = require('fs');
+// var data = '';
+//
+// // Create a readable stream
+// var readerStream = fs.createReadStream('input.txt');
+//
+// //Set the encoding to be utf8
+// readerStream.setEncoding('UTF8');
+//
+// //Handle stream events --> data, end, and error
+// readerStream.on('data', function(chunk){
+//   data += chunk;
+// })
+//
+// readerStream.on('end', function(){
+//   console.log(data);
+// });
+//
+// readerStream.on('error', function(err){
+//   console.log(err.stack);
+// })
+//
+// console.log("Program Ended");
+
+//Writing to a Stream
+
+// var fs = require("fs");
+// var data = 'Simply Easy Learning';
+//
+// //Create a writable stream
+// var writerStream = fs.createWriteStream('output.txt');
+//
+// //Write the data to stream with encoding to be utf8
+// writerStream.write(data, 'UTF8');
+//
+// //Mark the end of the file
+// writerStream.end();
+//
+// //Handle stream events --> finsih, and error
+// writerStream.on('finish', function(){
+//   console.log("Write completed.");
+// });
+//
+// writerStream.on('error', function(err){
+//   console.log(err.stack);
+// });
+//
+// console.log('Program Ended');
+
+//Piping Streams || reading from one stream and writing to another stream
+
+// var fs = require('fs');
+//
+// //create a readable stream
+// var readerStream = fs.createReadStream('input.txt');
+//
+// //create a writable stream
+// var writerStream = fs.createWriteStream('output.txt');
+//
+// //Pip the read and write operations
+// //read input.txt and write data to output.txt
+//
+// readerStream.pipe(writerStream);
+// console.log("Program Ended");
+
+// //Chaining the Streams
+// var fs = require("fs");
+// var zlib = require("zlib");
+
+// //Compress the file input.txt to input.txt.gz
+// fs.createReadStream('input.txt')
+//   .pipe(zlib.createGzip())
+//   .pipe(fs.createWriteStream('input.txt.gz'));
+// console.log("File Compressed.");
+
+// //Decompress the file input.txt to input.txt.gz
+// fs.createReadStream('input.txt.gz')
+//   .pipe(zlib.createGunzip())
+//   .pipe(fs.createWriteStream('input.txt'));
+//   console.log("File Decompressed");
+
+// var fs = require("fs");
+// Asynchronous read
+// fs.readFile('input.txt', function(err, data){
+//   if(err){
+//     return console.error(err)
+//   }
+//   console.log("Asynchronous read: "+ data.toString())
+// });
+//
+// // Synchronous read
+// var data = fs.readFileSync('input.txt');
+// console.log('Synchronous read: '+ data.toString());
+//
+// console.log("Program Ended");
+
+var fs = require("fs");
+
+// // Asynchronous - Opening File
+// console.log("Going to open file!");
+// fs.open('input.txt', 'r+', function(err, fd){
+//   if (err) {
+//     return console.error(err);
+//   }
+//   console.log("File opened successfully!");
+// });
+
+// console.log("Going to get file info!");
+// fs.stat('input.txt', function(err, stats){
+//   if(err){
+//     return console.error(err);
+//   }
+//   console.log(stats);
+//   console.log("Got file info successfully!");
+//
+//   //check file type
+//   console.log("isFile ? "+ stats.isFile());
+//   console.log("isDirectory ? " + stats.isDirectory());
+// });
+//write to a file // asynchronously read written data
+// console.log("Going to write into existing file");
+// fs.writeFile('input.txt', 'Simply Easy Learning', function(err){
+//   if(err){
+//     return console.error();
+//   }
+//
+//   console.log("Data written successfully!");
+//   console.log("Let's read newly written data");
+//
+//   fs.readFile('input.txt', function (err, data){
+//     if (err) {
+//       return console.console.error(err);
+//     }
+//     console.log("Asynchronous read: " + data.toString());
+//   });
+// });
+
+var buf = new Buffer(1024);
+console.log("Going to open an existing file");
+fs.open('input.txt', 'r+', function(err, fd) {
+  if(err){
+    return console.error(err);
+  }
+  console.log("File opened successfully!");
+  console.log("Going to read the file");
+  /*
+  fs.read(  fd, // this is the file descriptor returned by fs.open().
+            buffer, // this is the buffer that data will be writted to.
+            offset, // this is the offset in the buffer to start writing at.
+            length, // this is the number of bytes to read.
+            position, // this is an integer that specifies where to begin reading from. {0,null- data will be read from the current file position}
+            callback // this is the callback function which gets the 3 arguments, (err, bytesRead, buffer).
+  );
+  */
+  fs.read(fd, buf, 0, buf.length, 0, function(err, bytes){
+    if(err){
+      console.log(err);
+    }
+    console.log(bytes + " bytes read");
+
+    //Print only read bytes to avoid junk.
+    if(bytes > 0){
+      console.log(buf.slice(0, bytes).toString());
+    }
+
+    // Close the opened file.
+    fs.close(fd, function(err){
+      if (err){
+        console.log(err);
+      }
+      console.log("File closed successfully.")
+    });
+  });
+});
